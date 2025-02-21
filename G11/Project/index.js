62c20ee7215c68546d80c30a188435980c685149
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-02-21 10:10:30 +0530

    APIs of category management

--- G11/Project/index.js
+++ G11/Project/index.js
@@ -37,12 +37,13 @@ app.get("/api/category/:id", (req, resp) => {
 });
 
 //insert category in database
-app.post("/api/category",(req,resp)=>{
-    let body = req.body;
-    if(!body.cate_name){
-        return resp.status(404).json({"msg":"Mandotory field is missing."});
-    }
-    connection.query(`INSERT INTO master_category 
+app.post("/api/category", (req, resp) => {
+  let body = req.body;
+  if (!body.cate_name) {
+    return resp.status(404).json({ msg: "Mandotory field is missing." });
+  }
+  connection.query(
+    `INSERT INTO master_category 
         (cate_name, cate_desc, is_enable, created_on, created_by) 
         VALUES (
         '${body.cate_name}',
@@ -50,15 +51,76 @@ app.post("/api/category",(req,resp)=>{
         '${body.is_enable}',
         '${body.created_on}',
         '${body.created_by}'
-        )`,(error,result)=>{
-            if(error){
-                return resp.status(404).json({"msg":error});
-            }else{
-                return resp.status(200).json({"msg":"Record inserted successfully!"});
-            }
-    })
+        )`,
+    (error, result) => {
+      if (error) {
+        return resp.status(404).json({ msg: error });
+      } else {
+        return resp.status(200).json({ msg: "Record inserted successfully!" });
+      }
+    }
+  );
 });
 
+// Delete category
+app.delete("/api/category/:id", (req, resp) => {
+  let id = req.params.id;
+  connection.query(
+    "DELETE FROM master_category WHERE id = ${id}",
+    (err, result) => {
+      if (err) {
+        return resp.status("400").json({ msg: "Error in SQL" });
+      } else {
+        return resp.status("200").json({ msg: "Record deleted successfully." });
+      }
+    }
+  );
+});
+
+//Update few columns of any record
+app.patch("/api/category", (req, resp) => {
+  let body = req.body;
+  let id = body.id;
+  if (!body.cate_name) {
+    return resp.status("404").json({ msg: "Mandatory field is missing!" });
+  }
+  connection.query(
+    `UPDATE master_category SET cate_name = '${cate_name}', cate_desc ='${cate_desc}' 
+    WHERE id = ${id}`,
+    (err, result) => {
+      if (err) {
+        return resp.status("400").json({ msg: err });
+      } else {
+        return resp.status("200").json({ msg: "Record updated successfully" });
+      }
+    }
+  );
+});
+
+//update the entire category
+app.put("/api/category", (req, resp) => {
+  let body = req.body;
+  let id = body.id;
+  if (!body.cate_name) {
+    return resp.status("404").json({ msg: "Mandatory field is missing!" });
+  }
+  connection.query(
+    `UPDATE master_category SET 
+    cate_name = '${cate_name}', 
+    cate_desc ='${cate_desc}',
+    is_enable = '${is_enable}',
+    created_on = '${created_on}',
+    created_by = '${created_by}'
+    WHERE id = ${id}`,
+    (err, result) => {
+      if (err) {
+        return resp.status("400").json({ msg: err });
+      } else {
+        return resp.status("200").json({ msg: "Record updated successfully" });
+      }
+    }
+  );
+});
 
 app.listen(3000, () => {
   console.log("App is running on port 3000");
