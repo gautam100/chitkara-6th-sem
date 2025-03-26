40d653fafb2e4e78462ef6130f666f4d1aa409c6
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-03-26 18:46:49 +0530

    MVC

---
+++ G30/MVC/src/models/categoryModel.js
@@ -0,0 +1,17 @@
+const connection = require("../config/connection");
+
+class categoryModel {
+  static categoryList() {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT * FROM master_category", (error, rows) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  } //function
+} // class end
+
+module.exports = categoryModel;
