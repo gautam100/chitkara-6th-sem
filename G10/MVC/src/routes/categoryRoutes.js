84038563f37b2de38b2ab862e00efaf2ca866867
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-04-01 23:25:10 +0530

    steps of MVC

--- G10/MVC/src/routes/categoryRoutes.js
+++ G10/MVC/src/routes/categoryRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getCategoryList, saveCategory} = require("../controllers/categoryController");
+
 router.get("/categoryList", getCategoryList);
 router.post("/categorySave",saveCategory);
 
